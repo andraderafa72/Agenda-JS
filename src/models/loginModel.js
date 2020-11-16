57092b14e1bce8c1756dfa6583461454be93abae
andraderafa <andraderafa@users.github.com>
2020-11-16 10:14:45 -0300

    Versão 0.2.0
    0.1.X
    - Era necessário fazer login para editar a agenda, sendo unica para todas as sessões.
    
    0.2.X
    - Cada usuário possui sua própria agenda.
    
    Update contatoModel.js,
    Update loginModel.js,
    Update contatosTable.ejs,
    Update formContato.ejs.

--- src/models/loginModel.js
+++ src/models/loginModel.js
@@ -7,7 +7,6 @@ const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   nome: { type: String, required: true },
-  contatos: { type: Number, required: true, default: 0 }
 });
 
 // MODEL USADO PARA CRUD
